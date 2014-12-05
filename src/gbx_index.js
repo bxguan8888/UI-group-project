26a61dd9a0d77733cbd92939704b639565a73862
Boxuan Guan <boxuanguan@dyn-160-39-146-99.dyn.columbia.edu>
2014-12-04 22:46:31 -0500

    seperate script

---
+++ src/gbx_index.js
@@ -0,0 +1,39 @@
+function getBestList(){
+	var Book_api = "22167e35f2cd71ee36835bba032fee38:1:70162819";
+
+	var url = "http://api.nytimes.com/svc/books/v3/lists/names.jsonp?api-key="+Book_api;
+
+	
+
+	$.ajax({
+		'url': url,
+		'method': 'GET',
+		'cache': true,
+		'dataType': 'jsonp',
+		'success': function(data, textStats, XMLHttpRequest){
+			console.log(data);
+
+
+			list;
+			
+
+
+		}
+	});
+
+	// var url = "http://api.nytimes.com/svc/books/v3/reviews.jsonp?callback=books&title=REVIVAL&api-key="+Book_api;
+
+	// $.ajax({
+	// 	'url': url,
+	// 	'method': 'GET',
+	// 	'jsonpCallback' : 'books',
+	// 	'cache': true,
+	// 	'dataType': 'jsonp',
+	// 	'success': function(data, textStats, XMLHttpRequest){
+	// 		console.log(data);
+	// 	}
+	// });
+
+}
+
+
