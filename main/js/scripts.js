788dd63f44c33089f7fb1e1e16b9c7bb4c585773
eddiesong <eddie@dyn-160-39-178-79.dyn.columbia.edu>
2014-12-06 18:24:32 -0500

    add a bookmark in html

--- main/js/scripts.js
+++ main/js/scripts.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
+
+function add_favo(arg){
+	var ele;
+	ele = "favo_icon" + arg;
+
+	//check global varable favorite
+	//if added then remove from favorite
+	//if not add to favorite
+
+	document.getElementById(ele).style.backgroundImage = "url(../main/images/bookmark-after.png)";
+}
