0e114f1ce55e973d54c1bb984c8c65a8a7b9eeed
AlexandraVon <305120283@qq.com>
2014-12-08 23:33:13 -0500

    update
    update

--- main/js/individual.js
+++ main/js/individual.js
@@ -1,59 +1,28 @@
-var datainurl = $.url().param('data');
-console.log(datainurl);
-var splitTemp = datainurl.split("bookimage:");
-splitTemp = splitTemp[1].split(",bookname:");
-var bookimage = splitTemp[0];
-splitTemp = splitTemp[1].split(",bookauthor:");
-var bookname = splitTemp[0];
-splitTemp = splitTemp[1].split(",bookdesc:");
-var bookauthor = splitTemp[0];
-splitTemp = splitTemp[1].split(",bookpublisher:");
-var bookdesc = splitTemp[0];
-splitTemp = splitTemp[1].split(",bookisbn:");
-var bookpublisher = splitTemp[0];
-splitTemp = splitTemp[1].split(",bookamazon:");
-var bookisbn = splitTemp[0];
-splitTemp = splitTemp[1].split(",bookcurrentrank:");
-var bookamazon = splitTemp[0];
-splitTemp = splitTemp[1].split(",booklastrank:");
-var bookcurrentrank = splitTemp[0];
-var booklastrank = splitTemp[1];
-var currentBook = {
-	"bookimage":bookimage,
-	"bookname":bookname,
-	"bookauthor":bookauthor,
-	"bookdesc":bookdesc,
-	"bookpublisher":bookpublisher,
-	"bookisbn":bookisbn,
-	"bookamazon":bookamazon,
-	"bookcurrentrank":bookcurrentrank,
-	"booklastrank":booklastrank
-};
-console.log(currentBook);
+var book = store.get('book_detail');
+console.log(book);
 $( document ).ready(function() {
-  	individualDetail(currentBook);
+  	individualDetail(book);
 });
 
 function individualDetail(bookObject){
 	$("#detailImg").empty();
-	$("#detailImg").append("<img src=\""+bookObject.bookimage+"\" style=\"display:inline; margin:6px\" width=\"90%\">");
+	$("#detailImg").append("<img src=\""+bookObject.book_image+"\" style=\"display:inline; margin:6px\" width=\"90%\">");
 	$("#detail-title").empty();
-	$("#detail-title").append(bookObject.bookname);
+	$("#detail-title").append(bookObject['title']);
 	$("#detail-author").empty();
-	$("#detail-author").append("by "+bookObject.bookauthor);
+	$("#detail-author").append("by "+bookObject.author);
 	$("#detail-desc").empty();
-	$("#detail-desc").append(bookObject.bookdesc);
+	$("#detail-desc").append(bookObject['description']);
 	$("#detail-publisher").empty();
-	$("#detail-publisher").append(bookObject.bookpublisher);
+	$("#detail-publisher").append(bookObject.publisher);
 	$("#detail-isbn").empty();
-	$("#detail-isbn").append("ISBN: "+bookObject.bookisbn);
+	$("#detail-isbn").append("ISBN: "+bookObject.primary_isbn13);
 	$("#buy").empty();
-	if(bookObject.bookamazon!='undefined'){
-		$("#buy").append("<a href=\""+bookObject.bookamazon+"\" target=_blank><img src=\"images/Amazon-Logo.png\" width=\"14%\">");
+	if(bookObject.amazon_product_url!=null){
+		$("#buy").append("<a href=\""+bookObject.amazon_product_url+"\" target=_blank><img src=\"images/Amazon-Logo.png\" width=\"14%\">");
 	}
-	//var chartObject = [{color:"#6c8dbb",data:[[1,bookObject.bookcurrentrank],[2,bookObject.booklastrank]]}];
-	//var option = { xaxis: { max: 5, ticks: [[1, "Last"],[2, "Current"]] },yaxis: {transform: function (v) { return -v; },inverseTransform: function (v) { return -v; }} };
-	//$.plot($("#ranktrend"),[{color:"#6c8dbb",data:[[1,bookObject.bookcurrentrank],[2,bookObject.booklastrank]]}],{ xaxis: { max: 5, ticks: [[1, "Last"],[2, "Current"]] },yaxis: {transform: function (v) { return -v; },inverseTransform: function (v) { return -v; }} });
+
+	store.remove('book_detail');
 }
 
 
